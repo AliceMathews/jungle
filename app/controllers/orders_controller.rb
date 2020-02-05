a9180624b54fce2cbc2a69d27ef5218324c8d1ef
AliceMathews <alice_mathews@hotmail.co.uk>
2020-02-05 20:00:20 +0000

    gets all product data for each line item

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,20 +1,27 @@
 class OrdersController < ApplicationController
-
   def show
     @order = Order.find(params[:id])
+
+    @cart = {}
+    @order.line_items.each { |el| @cart[el["product_id"]] = el["quantity"] }
+    puts "cart: #{@cart}"
+
+    @product_ids = @order.line_items.map { |el| el.product_id }
+    @enhanced_order ||= Product.where(id: @cart.keys).map { |product| { product: product, quantity: @cart[product.id] } }
+
+    puts "order: #{@enhanced_order.inspect}"
   end
 
   def create
     charge = perform_stripe_charge
-    order  = create_order(charge)
+    order = create_order(charge)
 
     if order.valid?
       empty_cart!
-      redirect_to order, notice: 'Your Order has been placed.'
+      redirect_to order, notice: "Your Order has been placed."
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
     end
-
   rescue Stripe::CardError => e
     redirect_to cart_path, flash: { error: e.message }
   end
@@ -28,10 +35,10 @@ class OrdersController < ApplicationController
 
   def perform_stripe_charge
     Stripe::Charge.create(
-      source:      params[:stripeToken],
-      amount:      cart_subtotal_cents,
+      source: params[:stripeToken],
+      amount: cart_subtotal_cents,
       description: "Khurram Virani's Jungle Order",
-      currency:    'cad'
+      currency: "cad",
     )
   end
 
@@ -49,11 +56,10 @@ class OrdersController < ApplicationController
         product: product,
         quantity: quantity,
         item_price: product.price,
-        total_price: product.price * quantity
+        total_price: product.price * quantity,
       )
     end
     order.save!
     order
   end
-
 end
