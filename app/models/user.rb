c3201e95f5df1318d2d3eac11d6457b8dcc7a0ce
AliceMathews <alice_mathews@hotmail.co.uk>
2020-02-11 02:20:26 +0000

    made email field lowercase to make case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,20 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true, presence: true
   validates :name, presence: true
 
+  before_save :downcase_fields
+
   def self.authenticate_with_credentials(email, password)
-    @user = User.find_by_email(email)
+    @user = User.find_by_email(email.downcase)
 
     if @user && @user.authenticate(password)
       @user
     else
       nil
     end
+  end
+
+  def downcase_fields
+    self.email.downcase!
   end
 
   # helper_method :authenticate_with_credentials
