b0a8398ffaebb8aa36e9bc96ef6022b6e60268aa
AliceMathews <alice_mathews@hotmail.co.uk>
2020-02-11 01:55:22 +0000

    added class method to user for authentication check. updated controller to use result of this

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:email])
-
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
+      session[:user_id] = user.id
       redirect_to "/"
-    elsif @user && !@user.authenticate(params[:password])
-      flash[:alert] = "Incorrect password"
-      redirect_to "/login"
-    elsif !@user
-      flash[:alert] = "User does not exist"
+    else
+      flash[:alert] = "Incorrect username or password"
       redirect_to "/login"
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,15 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true, presence: true
   validates :name, presence: true
 
-  def authenticate_with_credentials(email, password)
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by_email(email)
+
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
   end
+
+  # helper_method :authenticate_with_credentials
 end
