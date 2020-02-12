6b629f959b20f9253866535063ca84fa2563406d
AliceMathews <alice_mathews@hotmail.co.uk>
2020-02-11 21:02:42 -0800

    added base controller to handle admin auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
-class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
+class Admin::DashboardController < Admin::BaseController
+  # http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
 
   def show
     @products_count = Product.count
