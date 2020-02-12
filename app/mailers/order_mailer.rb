aa8da8d6b904657d9dbb9367c660a1caa102b68c
AliceMathews <alice_mathews@hotmail.co.uk>
2020-02-12 22:32:42 +0000

    updated order create action to call the mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,7 +1,9 @@
 class OrderMailer < ApplicationMailer
   default from: "no-reply@jungle.com"
 
-  def order_confirmation(order_id, user_email)
-    mail(to: email, subject: "Order confirmation - #{order_id}")
+  def order_confirmation()
+    @email = params[:user_email]
+    @order_id = params[:order_id]
+    mail(to: @email, subject: "Order confirmation - #{@order_id}")
   end
 end
