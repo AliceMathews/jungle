5d5ed52efd74de0f05cdb11a250174a735df7cdd
AliceMathews <alice_mathews@hotmail.co.uk>
2020-02-13 00:00:12 +0000

    added letter_opener gem to test email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,9 +1,9 @@
 class OrderMailer < ApplicationMailer
   default from: "no-reply@jungle.com"
 
-  def order_confirmation()
-    @email = params[:user_email]
-    @order_id = params[:order_id]
+  def order_confirmation(email, order_id)
+    @email = email
+    @order_id = order_id
     mail(to: @email, subject: "Order confirmation - #{@order_id}")
   end
 end
--- test/mailers/previews/order_mailer_preview.rb
+++
@@ -1,6 +0,0 @@
-class OrderMailerPreview < ActionMailer::Preview
-  def order_confirmation
-    # UserMailer.with(user: User.first).welcome_email
-    OrderMailer.with(user_email: @order.email, order_id: @order.id).order_confirmation
-  end
-end
